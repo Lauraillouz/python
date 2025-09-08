710e25b8af924f0b91803a48ee8cddaa2fa9306d
Laura Illouz <lauraillouz@mac-18.home>
2025-09-08 10:06:44 +0200

    feat: create a class

---
+++ class.py
@@ -0,0 +1,31 @@
+class ToolBox:
+    def __init__(self):
+        self.tools = []
+
+    def add_tool(self, tool):
+        self.tools.append(tool)
+
+    def remove_tool(self, tool):
+        self.tools.remove(tool)
+
+
+class Hammer:
+    def __init__(self, color="red"):
+        self.color = color
+
+    def nail_in(self, nail):
+        nail.nail_in()
+
+    def nail_out(self, nail):
+        nail.nail_out()
+
+
+class Screwdriver:
+    def __init__(self, size_in_mm=2):
+        self.size_in_mm = size_in_mm
+
+    def screw_in(self, screw):
+        screw.screw_in()
+
+    def screw_out(self, screw):
+        screw.screw_out()
